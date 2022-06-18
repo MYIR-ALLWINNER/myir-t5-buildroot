e306c7a51ff83465d4c051ba06f0c38b522b29b9
licy <licy.li@myirtech.com>
2022-06-18 10:59:27 +0800

    release: MYD-YT507H_buildroot201902-RC0

--- board/olimex/a10_olinuxino/post-build.sh
+++ board/olimex/a10_olinuxino/post-build.sh
@@ -1,1 +1,8 @@
-../a20_olinuxino/post-build.sh
+#!/bin/sh
+
+cp $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
+
+if [ -e $BINARIES_DIR/script.bin ]; then
+	# mali requires a legacy kernel
+	cp $BINARIES_DIR/script.bin $TARGET_DIR/boot/script.bin
+fi
